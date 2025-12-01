6d95ecafc09c0c7685dae7e3df461caafb24c0b3
Tomislav Dukez <tomdu3@ymail.com>
2025-12-01 22:42:37 +0000

    add aoc_make bash

---
+++ 2025/aoc_make.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if the correct number of parameters is provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <day_number>"
+  exit 1
+fi
+
+DAY_NUMBER=$1
+DIRECTORY="./day$DAY_NUMBER"
+
+# Create the directory
+mkdir -p "$DIRECTORY"
+
+# Create the files
+touch "$DIRECTORY/README.md"
+touch "$DIRECTORY/day${DAY_NUMBER}part1.py"
+touch "$DIRECTORY/day${DAY_NUMBER}part2.py"
+touch "$DIRECTORY/input.txt"
+
+echo "Directory and files created successfully in $DIRECTORY"
