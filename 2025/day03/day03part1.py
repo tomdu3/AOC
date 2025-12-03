eac0e4d913244d7d048ed6fc98c1c5aaceadd739
Tomislav Dukez <tomdu3@ymail.com>
2025-12-03 07:33:04 +0000

    fix: correct AOC 25 day3 part 1

--- 2025/day03/day03part1.py
+++ 2025/day03/day03part1.py
@@ -3,14 +3,13 @@ import re
 
 # read input
 with open('input.txt') as f:
-    lines = f.readlines()
-    for line in lines:
-        line = line.strip().split()
+    lines = [line.strip() for line in f.readlines()]
+
 
 
 def max_joltage(line):
     highest = 0
-    for i, num in enumerate(line[:-1]):
+    for i, num in enumerate(line):
         for j, num2 in enumerate(line[i+1:]):
             if int(num+num2) > highest:
                 highest = int(num+num2)
